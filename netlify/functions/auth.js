3b4137e63b18377c153516771cb9e2342869132e
REMEX0 <102753256+REMEX0@users.noreply.github.com>
2025-08-28 13:39:16 +0300

    Update auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -7,8 +7,10 @@ exports.handler = async function(event, context) {
 
     try {
         const { action, email, password } = JSON.parse(event.body);
-        const MONGODB_URI = "mongodb+srv://stararmx_db_user:mOJAN8MDZCrYeoAj@cluster0.kvpxrlu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";;
-        const DB_NAME = process.env.DB_NAME || 'stararmx_db_user';
+        
+        // استخدم متغير البيئة بدلاً من السلسلة الثابتة
+        const MONGODB_URI = "mongodb+srv://stararmx_db_user:mOJAN8MDZCrYeoAj@cluster0.kvpxrlu.mongodb.net/myapp?retryWrites=true&w=majority&appName=Cluster0";
+        const DB_NAME = 'myapp';
         
         if (!MONGODB_URI) {
             throw new Error('MongoDB connection string not found');
@@ -71,6 +73,4 @@ exports.handler = async function(event, context) {
             body: JSON.stringify({ success: false, message: 'Server error: ' + error.message })
         };
     }
-
 };
-
